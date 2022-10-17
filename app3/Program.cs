00598f3960232e5c2dee136e6f638d8d7b607534
Aivenest <aivenest@gmail.com>
2022-10-17 23:34:33 +0500

    app3

---
+++ app3/Program.cs
@@ -0,0 +1,67 @@
+﻿int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetNumberValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = GetNumberValue(minValue, maxValue);
+        }
+    }
+}
+
+void ArithmeticMeanOfTheColumn(int[,] arr)
+{   
+    double sum =0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum=sum+arr[j,i];
+            if (j==(arr.GetLength(0)-1))
+            {
+                Console.WriteLine($"Среднее арифметическое {i+1} стобца равняется {Math.Round(sum/(j+1),1)}");
+                sum=0;
+            }
+        }
+    }
+    
+}
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m, n];
+}
+
+int m = GetNumberFromConsole("Введите размерность массива: m");
+int n = GetNumberFromConsole("Введите размерность массива: n");
+int[,] array = InitializateArray(m, n);
+int maxValue = GetNumberFromConsole("Введите максимальное допустимое число в массиве:");
+int minValue = GetNumberFromConsole("Введите минимальное допустимое число в массиве:");
+FillArray(array, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+ArithmeticMeanOfTheColumn(array);
